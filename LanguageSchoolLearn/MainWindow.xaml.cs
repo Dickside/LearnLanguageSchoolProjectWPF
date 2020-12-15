7cb89e9fb77ee3197f5dabc386a7d9d2ad4a8128
Mat Marsh <giktoruno@gmail.com>
2020-12-15 19:03:37 +0400

    Nixuya ne sdelal

--- LanguageSchoolLearn/MainWindow.xaml.cs
+++ LanguageSchoolLearn/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LanguageSchoolLearn.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,7 +34,31 @@ namespace LanguageSchoolLearn
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             var login = LoginBox.Text;
-            var passwod = PasswodBox.Password;
+            var password = PasswodBox.Password;
+            var user = App.Entities.Client.FirstOrDefault(x=> x.FirstName == login && x.LastName == password);
+            //Viktor 4321
+            //Den 1234
+            if (user != null)
+            {
+                if (user.GenderCode == "м")
+                {
+                    var view = new AdminView();
+                    this.Hide();
+                    view.ShowDialog();
+                    this.Close();
+                }
+                else
+                {
+                    var view = new UserView();
+                    this.Hide();
+                    view.ShowDialog();
+                    this.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль");
+            }
         }
     }
 }
